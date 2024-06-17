08ed8ae2dea206884e6757aceef15df93ef3790d
Joy02-Rolyn15 <aca211809@gmail.com>
2024-06-17 21:53:19 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                '2xs': '10px',
+              },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+    darkMode: ['selector', '[data-mode="dark"]'],
+};
